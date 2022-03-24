286c66d5a4ca25010f00b5ee71c8fbf1b5331db0
Fellipe Augusto <Fellipe246@hotmail.com>
2022-03-24 17:57:28 -0300

    Prova-GFT

---
+++ SegundaQuestao/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace segundaQuestao
+{
+    class Program
+    {
+        static void Main()
+        {
+            var quantidadeImcomoda;
+            int numero;
+            
+            Console.WriteLine("Digite um numero: ");
+            int numero = int.Parse(Console.ReadLine());
+            
+            while (quantidadeImcomoda == numero)
+            {
+                Console.WriteLine("muito mais");
+            }
+            
+
+
+
+
+            
+            
+    
+
+        
+
+
+
+        }
+    }
+}
