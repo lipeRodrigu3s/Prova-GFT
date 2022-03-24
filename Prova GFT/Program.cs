0f6dbc8219e864e651c2aecd90351b85aa1cdc28
Fellipe Augusto <Fellipe246@hotmail.com>
2022-03-24 17:13:55 -0300

    Primeira Questão

---
+++ Prova GFT/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PrimeiraQuestao
+{
+    class Program
+    {
+        static void Main()
+        {
+            int senha;
+            
+            Console.WriteLine("Digite a Senha");
+            senha = int.Parse(Console.ReadLine());
+
+            while(senha != 2018)
+            {
+                Console.WriteLine("Senha invalida, digite novamente");
+                senha = int.Parse(Console.ReadLine());
+            }
+                Console.WriteLine("Acesso Garantido");    
+            
+        }
+    }
+}
